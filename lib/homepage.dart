431c7b4def2ba498d5fbb80d0fd72fa62dff65bd
Aryan <aryan.khubchandani@gmail.com>
2021-09-30 01:53:54 +0530

    add home page

---
+++ lib/homepage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Widget Catalog"),
+      ),
+      backgroundColor: Colors.grey[900],
+      body: getWidgetList(),
+    );
+  }
+
+  ListView getWidgetList() {
+    TextStyle titleStyle = TextStyle(
+      color: Colors.white,
+    );
+
+    CircleAvatar arrow = CircleAvatar(
+      child: Icon(
+        Icons.keyboard_arrow_right,
+        color: Colors.white,
+      ),
+      backgroundColor: Colors.orange[900],
+    );
+
+    return ListView(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      children: [
+        ListTile(
+          leading: arrow,
+          title: Text(
+            "Widget 1",
+            style: titleStyle,
+          ),
+        ),
+        ListTile(
+          leading: arrow,
+          title: Text(
+            "Widget 2",
+            style: titleStyle,
+          ),
+        ),
+        ListTile(
+          leading: arrow,
+          title: Text(
+            "Widget 3",
+            style: titleStyle,
+          ),
+        ),
+        ListTile(
+          leading: arrow,
+          title: Text(
+            "Widget 4",
+            style: titleStyle,
+          ),
+        ),
+        ListTile(
+          leading: arrow,
+          title: Text(
+            "Widget 5",
+            style: titleStyle,
+          ),
+        ),
+      ],
+    );
+  }
+}
