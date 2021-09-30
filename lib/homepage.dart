827ce46db01ce42043d8992a6e3674b1a8058dec
Aryan <aryan.khubchandani@gmail.com>
2021-09-30 13:20:55 +0530

    add image widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:fludget/routes/icon.dart';
+import 'package:fludget/routes/image.dart';
 import 'package:fludget/routes/text.dart';
 import 'package:flutter/material.dart';
 
@@ -33,7 +34,7 @@ class HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.orange[900],
     );
-
+    TextStyle subtitleStyle = TextStyle(color: Colors.white70);
     return ListView(
       padding: EdgeInsets.symmetric(vertical: 10.0),
       children: [
@@ -74,9 +75,23 @@ class HomePageState extends State<HomePage> {
         ListTile(
           leading: arrow,
           title: Text(
-            "Widget 3",
+            "Image Widget",
             style: titleStyle,
           ),
+          subtitle: Text(
+            "Asset Image, Network Image",
+            style: subtitleStyle,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return ImageSample();
+                },
+              ),
+            );
+          },
         ),
         ListTile(
           leading: arrow,
---
+++ lib/routes/image.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ImageSample extends StatelessWidget {
+  const ImageSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image"),
+      ),
+      backgroundColor: Colors.grey[900],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Image from assets:",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.asset("assets/images/meme.png"),
+            SizedBox(height: 15.0),
+            Text(
+              "Image from internet:",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://uploads-ssl.webflow.com/5ee12d8e99cde2e20255c16c/5ef24ca36ab1f5eda546e719_flutter-meme-guarantee-it.jpg"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
