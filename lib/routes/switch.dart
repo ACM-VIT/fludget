2228b7b921a4462106c543910fd198f682fa4b3d
shiv2299 <shiv2299@gmail.com>
2021-10-04 20:45:36 +0530

    change background color in implementation tab and added description

--- lib/routes/switch.dart
+++ lib/routes/switch.dart
@@ -17,8 +17,18 @@ class _SwitchSampleState extends State<SwitchSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       body: Center(
-        child: Switch(value: _isOn, onChanged: _toggleSwitch),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Tap on the switch to toggle",
+              style: TextStyle(color: Colors.white),
+            ),
+            Switch(value: _isOn, onChanged: _toggleSwitch),
+          ],
+        ),
       ),
     );
   }
@@ -29,14 +39,27 @@ class SwitchDescription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            "Switch Widget in Flutter",
+            "Switch",
             style: TextStyle(
                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          Text(
+            """Switch widget in Flutter s used to show a toggle state like ON and OFF.
+It has two major arguments:
+
+=> bool value:  Used to show current state of the switch. true for ON and false for OFF.
+
+=> void Function onChange(bool):  Function used to change the state of the switch when user taps on the widget. It gives one parameter which is the new value for the switch.""",
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
           SizedBox(
             height: 20,
