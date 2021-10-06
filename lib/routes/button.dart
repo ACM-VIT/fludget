64525fdca391a809aba6df234afeea2053fc84a2
Dario Castellano <dario.castellano@etuitus.it>
2021-10-06 19:12:12 +0200

    feat: add scrollable to buttons view

--- lib/routes/button.dart
+++ lib/routes/button.dart
@@ -12,70 +12,84 @@ class ButtonImplementation extends StatelessWidget {
       backgroundColor: Colors.grey[900],
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Outlined Button:",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Outlined Button:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-            OutlinedButton(
+              OutlinedButton(
+                  onPressed: () => _message(context),
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.orange[900],
+                  ),
+                  child: Text("Outlined Button")),
+              SizedBox(height: 50.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Elevated Button:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () => _message(context),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange[900],
+                  ),
+                  child: Text("Elevated Button")),
+              SizedBox(height: 50.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Text Button:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              TextButton(
                 onPressed: () => _message(context),
-                style: OutlinedButton.styleFrom(
+                style: TextButton.styleFrom(
                   primary: Colors.orange[900],
                 ),
-                child: Text("Outlined Button")),
-            SizedBox(height: 20.0),
-            Text(
-              "Elevated Button:",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+                child: Text("Text Button"),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () => _message(context),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.orange[900],
+              SizedBox(height: 50.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Floating Action Button:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                child: Text("Elevated Button")),
-            SizedBox(height: 20.0),
-            Text(
-              "Text Button:",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
               ),
-            ),
-            TextButton(
-              onPressed: () => _message(context),
-              style: TextButton.styleFrom(
-                primary: Colors.orange[900],
+              FloatingActionButton(
+                onPressed: () => _message(context),
+                backgroundColor: Colors.orange[900],
+                tooltip: ("Hold to show tooltip"),
+                child: Icon(
+                  Icons.add,
+                  size: 40.0,
+                ),
               ),
-              child: Text("Text Button"),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              "Floating Action Button:",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            FloatingActionButton(
-              onPressed: () => _message(context),
-              backgroundColor: Colors.orange[900],
-              tooltip: ("Hold to show tooltip"),
-              child: Icon(
-                Icons.add,
-                size: 40.0,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
