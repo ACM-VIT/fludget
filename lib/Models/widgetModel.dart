96fe9152c373f24bc99451fb5fe5a5214f07cb64
suryaa62 <suryaa62@gmail.com>
2021-10-09 00:31:30 +0530

    Feature added : filter widget by category

--- lib/Models/widgetModel.dart
+++ lib/Models/widgetModel.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/widgets.dart';
 
+enum WidgetCategoy {
+  Layout,
+  Accessibility,
+  Async,
+  Basics,
+  Cupertino,
+  Input,
+  Material,
+  Scrolling,
+  Styling,
+  Text,
+  Animation,
+  Assets,
+  Interaction,
+  Painting,
+  Effects,
+}
+
 class WidgetModel {
   final String name, subtitle, link;
   final Widget implementation;
   final Widget description;
-  const WidgetModel({
-    required this.name,
-    this.subtitle = '',
-    required this.link,
-    required this.implementation,
-    required this.description,
-  });
+  final List<WidgetCategoy> category;
+  const WidgetModel(
+      {required this.name,
+      this.subtitle = '',
+      required this.link,
+      required this.implementation,
+      required this.description,
+      required this.category});
 }
