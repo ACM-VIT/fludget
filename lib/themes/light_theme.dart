54be22238718e7cfbd5a8e60f8eefda643b5446b
jushutch <jushutch@umich.edu>
2021-10-11 18:41:43 -0400

    Create all theme classes

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fludget/themes/abstract_theme.dart';
+
+class LightTheme implements FludgetTheme {
+  @override
+  Color backgroundColor = Colors.white;
+
+  @override
+  Color accentColor = Colors.blue;
+
+  @override
+  Color fontColor = Colors.black;
+}
