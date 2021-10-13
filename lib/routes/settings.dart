fe045372b66e107958c3996b0409a46112f93dca
Kartik Gaddam <56345834+Karthick986@users.noreply.github.com>
2021-10-13 22:20:22 +0530

    added link

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -2,9 +2,15 @@
 import 'package:fludget/widgets_doc.dart';
 import 'package:fludget/themes/theme_switch.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsWidget extends StatelessWidget {
   final padding = EdgeInsets.symmetric(horizontal: 20);
+  
+  String _url = 'https://api.flutter.dev/flutter/widgets/widgets-library.html';
+  void _launchURL() async =>
+      await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
+  
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -20,7 +26,11 @@ class SettingsWidget extends StatelessWidget {
             ThemeSwitch(),
             ListTile(
               leading: FlutterLogo(size: 24.0,),
-              trailing: Icon(Icons.arrow_forward_ios_rounded),
+              trailing: InkWell(child: Icon(Icons.link,
+                size: 24.0,), onTap: () {
+                Navigator.pop(context);
+                _launchURL();
+              },),
               title: Text("Official Documentation"),
               onTap: (){
                 Navigator.pop(context);
