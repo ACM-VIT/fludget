13b299de9a96e98dab02aa1823a47661bf185632
Akshit Agarwal <akshitagarwal97@gmail.com>
2021-10-20 20:07:45 +0530

    Added Matrix Rotation and Compose Filter

--- lib/routes/BackdropFilter.dart
+++ lib/routes/BackdropFilter.dart
@@ -6,31 +6,133 @@ class BackdropFilterImplementation extends StatelessWidget {
   const BackdropFilterImplementation({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
     return Stack(
-      fit: StackFit.expand,
       children: <Widget>[
         Text('0' * 10000),
-        Center(
-          child: ClipRect(  // <-- clips to the 200x200 [Container] below
-            child: BackdropFilter(
-              filter: ImageFilter.blur(
-                sigmaX: 5.0,
-                sigmaY: 5.0,
-              ),
-              child: Container(
-                alignment: Alignment.center,
-                width: 200.0,
-                height: 200.0,
-                child: const Text('Hello World'),
+        Column(
+          children: <Widget>[
+            Center(
+              child: ClipOval(
+                // <-- clips to the 200x200 [Container] below
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 5.0,
+                    sigmaY: 5.0,
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 200.0,
+                    height: 200.0,
+                    child: const Text('Blur',style: TextStyle(fontSize: 50),),
+                  ),
+                ),
               ),
             ),
+            SizedBox(height: 10,),
+            Center(
+              child: ClipRect(
+                // <-- clips to the 200x200 [Container] below
+                child: BackdropFilter(
+                  filter: ImageFilter.matrix(Matrix4.rotationZ(0.2).storage),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 200.0,
+                    height: 200.0,
+                    child: const Text('Matrix Rotation',style: TextStyle(fontSize: 30),),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            Center(
+              child: ClipRect(
+                // <-- clips to the 200x200 [Container] below
+                child: BackdropFilter(
+                  filter: ImageFilter.compose(outer: ImageFilter.blur(sigmaX: 5.0,
+                    sigmaY: 5.0,), inner: ImageFilter.matrix(Matrix4.rotationZ(0.3).storage)),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 200.0,
+                    height: 200.0,
+                    child: const Text('Compose',style: TextStyle(fontSize: 30),),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          height: 500,
+          width: double.infinity,
+          child: Stack(
+            children: <Widget>[
+              Text('0' * 10000),
+              Center(
+                child: ClipRect(
+                  // <-- clips to the 200x200 [Container] below
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 5.0,
+                      sigmaY: 5.0,
+                    ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200.0,
+                      height: 200.0,
+                      child: const Text('Blur'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          height: 500,
+          width: double.infinity,
+          child: Stack(
+            children: <Widget>[
+              new ConstrainedBox(
+                  constraints: const BoxConstraints.expand(),
+                  child: new FlutterLogo()),
+              new Center(
+                child: new ClipOval(
+                  child: new BackdropFilter(
+                    filter: new ImageFilter.matrix(Matrix4.rotationZ(0.5).storage),
+                    child: new Container(
+                      width: 200.0,
+                      height: 200.0,
+                      decoration: new BoxDecoration(
+                          color: Colors.grey.shade200.withOpacity(0.5)),
+                      child: new Center(
+                        child: new Text('Frosted',
+                            style: Theme.of(context).textTheme.bodyText1),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
-    );
+    )));
   }
 }
-
 
 class BackdropFilterDescription extends StatelessWidget {
   const BackdropFilterDescription({Key? key}) : super(key: key);
