1d3b7b64d552e3d853ea2c95001e47cea61accee
Sachin Kumar <mail.sachin161@gmail.com>
2021-10-22 21:49:16 +0530

    Added different cursor colors and cursor widths to SelectableText Widget

--- lib/routes/SelectableText.dart
+++ lib/routes/SelectableText.dart
@@ -6,31 +6,79 @@ class SelectableTextImplementation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.1,
-        ),
-        Text(
-          'This Text is Not Selectable',
-          style: TextStyle(
-            fontSize: 24,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
           ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.1,
-        ),
-        SelectableText(
-          'This Text is Selectable',
-          style: TextStyle(
-            fontSize: 24,
+          Text(
+            'This Text is Not Selectable',
+            style: TextStyle(
+              fontSize: 24,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('Try selecting above text by long pressing'),
-        ),
-      ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          SelectableText(
+            'This Text is Selectable',
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Try selecting above text by long pressing'),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Tap on Below Text'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SelectableText(
+              'This Text has cursor color as purple.',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              cursorColor: Colors.purple,
+              cursorWidth: 3,
+              showCursor: true,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SelectableText(
+              'This Text has cursor color as blue.',
+              style: TextStyle(
+                fontSize: 28,
+              ),
+              textAlign: TextAlign.center,
+              cursorColor: Colors.blue,
+              cursorWidth: 5,
+              showCursor: true,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SelectableText(
+              'This Text has cursor color as red.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+              ),
+              cursorColor: Colors.red,
+              cursorWidth: 8,
+              showCursor: true,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -54,7 +102,16 @@ class SelectableTextCode extends CodeString {
   const SelectableTextCode();
   @override
   String buildCodeString() {
-    return """SelectableText('This Text is Selectable')
+    return """SelectableText(
+              'This Text has cursor color as purple.',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              cursorColor: Colors.purple,
+              cursorWidth: 3,
+              showCursor: true,
+              textAlign: TextAlign.center,
+            ),
 """;
   }
 }
