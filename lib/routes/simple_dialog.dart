f6dc7d6a13ce64e0ac21adf2880fc3a2dab1dcab
yuran09 <yuranfgs@gmail.com>
2021-10-24 00:28:14 +0200

    fixing simpleDialog.dart and widgetList.dart missing code

--- lib/routes/simple_dialog.dart
+++ lib/routes/simple_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:fludget/Models/codeString.dart';
 import 'package:flutter/material.dart';
 
 class SimpleDialogImp extends StatelessWidget {
@@ -105,5 +106,51 @@ class SimpleDialogDesc extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class SimpleDialogCode extends CodeString {
+  const SimpleDialogCode();
+  @override
+  String buildCodeString() {
+    return """Future<TimeOfDay?> showTimePicker({
+    required BuildContext context,
+    required TimeOfDay initialTime,
+    TransitionBuilder? builder,
+    bool useRootNavigator = true,
+    TimePickerEntryMode initialEntryMode = TimePickerEntryMode.dial,
+    String? cancelText,
+    String? confirmText,
+    String? helpText,
+    String? errorInvalidText,
+    String? hourLabelText,
+    String? minuteLabelText,
+    RouteSettings? routeSettings,
+  }) async {
+    assert(context != null);
+    assert(initialTime != null);
+    assert(useRootNavigator != null);
+    assert(initialEntryMode != null);
+    assert(debugCheckHasMaterialLocalizations(context));
+
+    final Widget dialog = TimePickerDialog(
+      initialTime: initialTime,
+      initialEntryMode: initialEntryMode,
+      cancelText: cancelText,
+      confirmText: confirmText,
+      helpText: helpText,
+      errorInvalidText: errorInvalidText,
+      hourLabelText: hourLabelText,
+      minuteLabelText: minuteLabelText,
+    );
+    return showDialog<TimeOfDay>(
+      context: context,
+      useRootNavigator: useRootNavigator,
+      builder: (BuildContext context) {
+        return builder == null ? dialog : builder(context, dialog);
+      },
+      routeSettings: routeSettings,
+    );
+  }""";
   }
 }
